f168a0cdcd931e21e56c4a731fb650cfc0d7cee6
Spyrius <61468175+Lubo77@users.noreply.github.com>
2023-09-19 11:27:59 +0200

    init

---
+++ template1.js
@@ -0,0 +1,19 @@
+// Load the custom font data from the font file
+import { font } from './Saans-Regular-normal.js';
+
+console.log(font);
+
+// Create a new jsPDF instance
+var doc = new jsPDF();
+
+// Add the custom font data???
+
+// Set the custom font
+doc.setFont("Saans-Regular");
+doc.setFontType("normal");
+
+// Add text using the custom font
+doc.text(20, 50, 'This is Saans Regular.');
+
+// Save the PDF
+doc.save('template1.pdf');
